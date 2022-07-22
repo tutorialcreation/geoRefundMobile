8e8b37ccde56cee80c25883c67918a1001d424d5
tutorialcreation <tutorialcreation81@gmail.com>
2022-07-22 16:20:00 +0300

    checking functions

--- contracts/refunder.sol
+++ contracts/refunder.sol
@@ -1,35 +1,76 @@
-pragma solidity >=0.4.25 <0.6.0;
+
+pragma solidity >=0.7.0 <0.9.0;
 
 contract refunder{
     enum StateType { Created, InTransit, Completed, OutOfCompliance}
     struct employee {
-        address identity,
-        uint256 latitude,
-        uint256 longitude,
-        string name,
-        string email
-    };
+        address identity;
+        uint256 latitude;
+        uint256 longitude;
+        string name;
+        string email;
+    }
+    uint256 range;
     employee[] public employees;
 
     address public employer;
+    bool public  complianceStatus;
     
-    function addEmployee(){}
+    function addEmployee(
+        address identity,
+        uint256 latitude,
+        uint256 longitude,
+        string memory name,
+        string memory email) public 
+    {
+        employees.push(employee(
+            identity,
+            latitude,
+            longitude,
+            name,
+            email
+        ));
+        
+    }
 
-    function removeEmployee(){}
+    function getEmployees() public returns(employee[] memory){
+        return employees;
+    }
 
-    function checkEmployeeExists(){}
 
-    function getLocation(){}
+    function removeEmployee(uint index) public returns(employee[] memory) {
+        
+        for (uint i = index; i<employees.length-1; i++){
+            employees[i] = employees[i+1];
+        }
+        delete employees[employees.length-1];
+        employees.length-1;
+        return employees;
+    }
+
+
     
-    function checkPosition(uint256 _latitude, uint256 _longitude){}
+    function checkPosition(uint256 _latitude, uint256 _longitude) public returns(bool) {
+        
+        if (_latitude < range){
+            complianceStatus = true;
+        }else{
+            complianceStatus = false;
+        }
 
-    function claimPayment(){}
+        return complianceStatus;
+    }
 
-    function getFeedback(){}
+    function claimPayment(uint256 _latitude, uint256 _longitude) public returns(bool){
+        
+        if (_latitude < range){
+            complianceStatus = true;
+        }else{
+            complianceStatus = false;
+        }
 
-    function reportCompliance(){}
-
-
+        return complianceStatus;
+    }
 
 
 }
