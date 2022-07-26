ea3565554e03fcb05b7c2b674ac61c902676ebef
tutorialcreation <tutorialcreation81@gmail.com>
2022-07-26 10:17:16 +0300

    started working on distance calculations

--- contracts/distance.sol
+++ contracts/distance.sol
@@ -1,6 +1,20 @@
 // Helper function defined outside of a contract
-function calculateDistance(uint x) pure returns (uint) {
-    return x * 2;
+pragma solidity ^0.8.4;
+
+contract DistanceCalculations {   
+    function callAddMod() public pure returns(uint){
+        return addmod(4, 5, 3);
+    }
+    function callMulMod() public pure returns(uint){
+        return mulmod(4, 5, 3);
+    }
+    function calculateDistance(uint x) public pure returns (uint) {
+        return x * 2;
+    }
+
+    function calculateSqrt(uint x) public  pure returns (uint y){
+        uint z = (x+1)/2;
+        
+    }
+
 }
-
-
